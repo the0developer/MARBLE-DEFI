75ff013f4d4edd84827a7647ee2791b612d35a30
VIER <kraiglyons0929@gmail.com>
2023-04-01 01:09:39 +0800

    add: announcement bar added

--- components/ConnectedWalletButton.tsx
+++ components/ConnectedWalletButton.tsx
@@ -1,61 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { Text } from './Text'
-import {
-  useBaseTokenInfo,
-  useTokenInfoFromAddress,
-} from '../hooks/useTokenInfo'
-import { getTokenBalance } from '../hooks/useTokenBalance'
-import {
-  convertMicroDenomToDenom,
-  formatTokenBalance,
-  convertToFixedDecimals,
-} from '../util/conversion'
-import { getTokenBalances } from 'util/token'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { CSS } from '@stitches/react'
-import styled from 'styled-components'
-import { Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react'
-import { Dialog, StyledCloseIcon } from 'components/Dialog'
 import { ArrowDown } from 'icons'
-import {
-  executeMultipleTransactions,
-  Transaction,
-  CONTRACT_NAME,
-  ONE_YOCTO_NEAR,
-} from 'util/near'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { getTokenBalance } from '../hooks/useTokenBalance'
+import { useBaseTokenInfo } from '../hooks/useTokenInfo'
+import { formatTokenBalance } from '../util/conversion'
+import { Text } from './Text'
 
 type ConnectedWalletButtonProps = { css?: CSS } & {
   walletName?: string
   onConnect: () => void
   onDisconnect: () => void
   connected: boolean
-}
-
-interface DepositedTokensInfo {
-  token_address: string
-  logoURI: string
-  symbol: string
-  amount: number
-}
-
-const tokensInfo = {
-  'wrap.near': {
-    id: 'wnear',
-    token_address: 'wrap.near',
-    symbol: 'wNEAR',
-    name: 'Wrapped Near',
-    decimals: 24,
-    logoURI: '/images/wnear.png',
-    icon: '/images/wnear.png',
-  },
-  'artex.marbledao.near': {
-    id: 'artex',
-    token_address: 'artex.marbledao.near',
-    symbol: 'ARTEX',
-    name: 'ARTEX',
-    decimals: 24,
-    logoURI: '/images/artex.png',
-    icon: '/images/artex.png',
-  },
 }
 
 export const ConnectedWalletButton = ({
@@ -66,24 +23,6 @@ export const ConnectedWalletButton = ({
 }: ConnectedWalletButtonProps) => {
   const baseToken = useBaseTokenInfo()
   const [balance, setBalance] = useState(0)
-  const [deposits, setDeposits] = useState<DepositedTokensInfo[]>([])
-  const [isShowing, setIsShowing] = useState(false)
-  useEffect(() => {
-    if (!connected) return
-    getTokenBalances().then((data) => {
-      const tokenAddresses = Object.keys(data)
-      const tokens = tokenAddresses.map((_tokenAddress) => {
-        return {
-          ...tokensInfo[_tokenAddress],
-          amount: convertMicroDenomToDenom(
-            data[_tokenAddress],
-            tokensInfo[_tokenAddress].decimals
-          ),
-        }
-      })
-      setDeposits(tokens)
-    })
-  }, [connected])
   const menuButtonCss = {
     background:
       'linear-gradient(180deg, rgba(255, 255, 255, 0.06) 0%, rgba(255, 255, 255, 0.06) 100%) !important',
@@ -128,33 +67,7 @@ export const ConnectedWalletButton = ({
       </ConnectWalletContainer>
     )
   }
-  const onRequestOpen = () => {
-    setIsShowing(true)
-  }
-  const onRequestClose = () => {
-    setIsShowing(false)
-  }
-  const handleWithdraw = async () => {
-    // const transactions: Transaction[] = []
-    const transactions: Transaction[] = deposits.map((_deposit) => {
-      const _transaction: Transaction = {
-        receiverId: CONTRACT_NAME,
-        functionCalls: [
-          {
-            methodName: 'withdraw',
-            args: {
-              token_id: _deposit.token_address,
-              amount: '0',
-            },
-            gas: '300000000000000',
-            amount: ONE_YOCTO_NEAR,
-          },
-        ],
-      }
-      return _transaction
-    })
-    executeMultipleTransactions(transactions)
-  }
+
   return (
     <>
       <Menu>
@@ -180,38 +93,11 @@ export const ConnectedWalletButton = ({
           </Text>
         </MenuButton>
         <MenuList css={menuListCss}>
-          <MenuItem css={menuItemCss} onClick={onRequestOpen}>
-            Your Account
-          </MenuItem>
           <MenuItem css={menuItemCss} onClick={onDisconnect}>
             Disconnect
           </MenuItem>
         </MenuList>
       </Menu>
-      <Dialog
-        isShowing={isShowing}
-        onRequestClose={onRequestClose}
-        kind="blank"
-      >
-        <StyledCloseIcon onClick={onRequestClose} offset={19} size="40px" />
-        <div>
-          <Title>Unused Tokens</Title>
-        </div>
-        <StyledDivForContent>
-          {deposits.map((_depositedToken, index) => (
-            <UnusedTokenItem key={index}>
-              <ImgDiv>
-                <img src={_depositedToken.logoURI} alt="token" />
-                {_depositedToken.symbol}
-              </ImgDiv>
-              <TokenValue>
-                {convertToFixedDecimals(_depositedToken.amount)}
-              </TokenValue>
-            </UnusedTokenItem>
-          ))}
-          <RefundButton onClick={handleWithdraw}>Withdraw</RefundButton>
-        </StyledDivForContent>
-      </Dialog>
     </>
   )
 }
@@ -236,42 +122,4 @@ const ConnectWalletContainer = styled.div`
   &:hover {
     opacity: 0.6;
   }
-`
-const Title = styled.div`
-  font-size: 20px;
-  font-family: Trajan;
-  color: white;
-`
-const StyledDivForContent = styled.div`
-  padding: 20px 0;
-`
-const UnusedTokenItem = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 20px;
-  color: white;
-
-  img {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: white;
-  }
-`
-const ImgDiv = styled.div`
-  display: flex;
-  align-items: center;
-  column-gap: 20px;
-`
-const TokenValue = styled.div``
-
-const RefundButton = styled(Button)`
-  box-shadow: 0px 10px 30px rgba(42, 47, 50, 0.2) !important;
-  backdrop-filter: blur(14px);
-  /* Note: backdrop-filter has minimal browser support */
-  border-radius: 20px !important;
-  height: 50px !important;
-  width: 170px !important;
-  color: black;
-  font-size: 14px !important;
 `
--- components/Layout/AppLayout.tsx
+++ components/Layout/AppLayout.tsx
@@ -8,22 +8,61 @@ import { ChakraProvider, HStack, Stack } from '@chakra-ui/react'
 import { FetchCoinInfo } from 'hooks/useTokenBalance'
 import { isPC } from 'util/device'
 import { getTokenBalances, TokenBalancesView } from 'util/token'
-import { convertMicroDenomToDenom } from 'util/conversion'
+import { Dialog, StyledCloseIcon } from 'components/Dialog'
+import {
+  convertMicroDenomToDenom,
+  convertToFixedDecimals,
+} from 'util/conversion'
+import { getCurrentWallet } from 'util/sender-wallet'
 import { SecondGradientBackground } from 'styles/styles'
 // import { useDb } from 'hooks/useDb'
-
+import {
+  executeMultipleTransactions,
+  Transaction,
+  CONTRACT_NAME,
+  ONE_YOCTO_NEAR,
+} from 'util/near'
 const tagManagerArgs = {
   gtmId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
 }
 
 //TagManager.initialize(tagManagerArgs)
+const tokensInfo = {
+  'wrap.near': {
+    id: 'wnear',
+    token_address: 'wrap.near',
+    symbol: 'wNEAR',
+    name: 'Wrapped Near',
+    decimals: 24,
+    logoURI: '/images/wnear.png',
+    icon: '/images/wnear.png',
+  },
+  'artex.marbledao.near': {
+    id: 'artex',
+    token_address: 'artex.marbledao.near',
+    symbol: 'ARTEX',
+    name: 'ARTEX',
+    decimals: 24,
+    logoURI: '/images/artex.png',
+    icon: '/images/artex.png',
+  },
+}
+interface DepositedTokensInfo {
+  token_address: string
+  logoURI: string
+  symbol: string
+  amount: number
+}
 
 export const AppLayout = ({
   footerBar = <FooterBar />,
   children,
   fullWidth = true,
 }) => {
-  const [deposits, setDeposits] = useState<TokenBalancesView>()
+  const { accountId } = getCurrentWallet()
+  const [deposits, setDeposits] = useState<DepositedTokensInfo[]>([])
+  const [isShowing, setIsShowing] = useState(false)
+  const [showAnnouncement, setShowAnnouncement] = useState(false)
   FetchCoinInfo()
   useEffect(() => {
     const _tempaccount = localStorage.getItem('accountId')
@@ -32,27 +71,111 @@ export const AppLayout = ({
       return
     }
     getTokenBalances().then((data) => {
-      setDeposits(data)
+      const tokenAddresses = Object.keys(data)
+      const tokens = tokenAddresses.map((_tokenAddress) => {
+        return {
+          ...tokensInfo[_tokenAddress],
+          amount: convertMicroDenomToDenom(
+            data[_tokenAddress],
+            tokensInfo[_tokenAddress].decimals
+          ),
+        }
+      })
+      setDeposits(tokens)
     })
-  }, [])
+  }, [accountId])
+  useEffect(() => {
+    if (!deposits) return
+    Object.values(deposits).forEach((_deposit) => {
+      if (_deposit.amount > 0.01) setIsShowing(true)
+    })
+  }, [deposits])
+
   // useDb()
   useEffect(() => {
     TagManager.initialize(tagManagerArgs)
   }, [])
-  const getBadgeInfo = () => {
-    const status = localStorage.getItem('accountId')
-    return '123'
+  const handleWithdraw = async () => {
+    // const transactions: Transaction[] = []
+    const transactions: Transaction[] = deposits.map((_deposit) => {
+      const _transaction: Transaction = {
+        receiverId: CONTRACT_NAME,
+        functionCalls: [
+          {
+            methodName: 'withdraw',
+            args: {
+              token_id: _deposit.token_address,
+              amount: '0',
+            },
+            gas: '300000000000000',
+            amount: ONE_YOCTO_NEAR,
+          },
+        ],
+      }
+      return _transaction
+    })
+    executeMultipleTransactions(transactions)
+  }
+  const onRequestOpen = () => {
+    setIsShowing(true)
+  }
+  const onRequestClose = () => {
+    setIsShowing(false)
   }
   return (
     <ChakraProvider>
       <StyledWrapper>
+        {isShowing && (
+          <AnnouncementBar>
+            <h1>You have some tokens left to withdraw</h1>
+            <WithdrawButton onClick={onRequestOpen}>Withdraw</WithdrawButton>
+          </AnnouncementBar>
+        )}
         {isPC() ? <NavigationSidebar /> : <FixedNavigationSidebar />}
-
         <StyledContainer>{children}</StyledContainer>
       </StyledWrapper>
+      <Dialog
+        isShowing={isShowing}
+        onRequestClose={onRequestClose}
+        kind="blank"
+      >
+        <StyledCloseIcon onClick={onRequestClose} offset={19} size="40px" />
+        <div>
+          <Title>Unused Tokens</Title>
+        </div>
+        <StyledDivForContent>
+          {deposits.map((_depositedToken, index) => (
+            <UnusedTokenItem key={index}>
+              <ImgDiv>
+                <img src={_depositedToken.logoURI} alt="token" />
+                {_depositedToken.symbol}
+              </ImgDiv>
+              <TokenValue>
+                {convertToFixedDecimals(_depositedToken.amount)}
+              </TokenValue>
+            </UnusedTokenItem>
+          ))}
+          <RefundButton onClick={handleWithdraw}>Withdraw</RefundButton>
+        </StyledDivForContent>
+      </Dialog>
     </ChakraProvider>
   )
 }
+
+const AnnouncementBar = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 40px;
+  background: linear-gradient(45deg, #eb5d47 0%, #a933b2 50%, #a4f9ff 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-family: Mulish;
+  }
+`
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -89,4 +212,54 @@ const StyledContainer = styled(SecondGradientBackground)`
   @media (max-width: 1024px) {
     margin-inline: 0 !important;
   }
+`
+const Title = styled.div`
+  font-size: 20px;
+  font-family: Trajan;
+  color: white;
+`
+const StyledDivForContent = styled.div`
+  padding: 20px 0;
+`
+const UnusedTokenItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  color: white;
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+  }
+`
+const ImgDiv = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 20px;
+`
+const TokenValue = styled.div``
+
+const RefundButton = styled.div`
+  cursor: pointer;
+  box-shadow: 0px 10px 30px rgba(42, 47, 50, 0.2) !important;
+  backdrop-filter: blur(14px);
+  /* Note: backdrop-filter has minimal browser support */
+  border-radius: 20px !important;
+  color: black;
+  font-size: 14px !important;
+  background: white;
+  width: fit-content;
+  padding: 10px 20px;
+`
+const WithdrawButton = styled.div`
+  padding: 5px 20px;
+  border-radius: 8px;
+  cursor: pointer;
+  background: white;
+  color: black;
+  font-family: Mulish;
+  font-size: 12px;
+  margin: 0 20px;
 `
